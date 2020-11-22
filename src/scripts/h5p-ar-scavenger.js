8397c025924e7aa4422a558d718de719cb7ef87f
Oliver Tacke <o.tacke@posteo.de>
2020-11-22 15:34:03 +0100

    Add en-US as fallback for h5p-php-reporting

--- src/scripts/h5p-ar-scavenger.js
+++ src/scripts/h5p-ar-scavenger.js
@@ -242,8 +242,12 @@ export default class ARScavenger extends H5P.Question {
       const definition = {};
       definition.name = {};
       definition.name[this.languageTag] = this.getTitle();
+      // Fallback for h5p-php-reporting, expects en-US
+      definition.name['en-US'] = definition.name[this.languageTag];
       definition.description = {};
       definition.description[this.languageTag] = this.getDescription();
+      // Fallback for h5p-php-reporting, expects en-US
+      definition.description['en-US'] = definition.description[this.languageTag];
 
       definition.type = 'http://adlnet.gov/expapi/activities/cmi.interaction';
       definition.interactionType = 'compound';
